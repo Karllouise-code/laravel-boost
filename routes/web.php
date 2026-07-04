227ee62a508740a4187ae765c2d295733ded8a31
Karl Rito <karllouiserito08@gmail.com>
2026-07-04 15:31:04 +0800

    feat: protect todo routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,12 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('todos', TodoController::class);
-Route::get('/download-sqlite', [DatabaseController::class, 'downloadSqlite'])->name('download.sqlite');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('todos', TodoController::class);
+    Route::get('/download-sqlite', [DatabaseController::class, 'downloadSqlite'])->name('download.sqlite');
 });
 
 require __DIR__.'/auth.php';
