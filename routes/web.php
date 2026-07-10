465432b6757a08d551a332917262aab073c21825
Karl Rito <karllouiserito08@gmail.com>
2026-07-10 17:05:03 +0800

    feat: add board routes, scope todo routes under boards

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BoardController;
 use App\Http\Controllers\DatabaseController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SharedBoardController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,8 +18,44 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/todos/export', [DatabaseController::class, 'export'])->name('todos.export');
-    Route::resource('todos', TodoController::class);
+
+    // Boards
+    Route::get('/boards', [BoardController::class, 'index'])->name('boards.index');
+    Route::post('/boards', [BoardController::class, 'store'])->name('boards.store');
+    Route::get('/boards/{slug}', [BoardController::class, 'show'])
+        ->middleware('board.access')->name('boards.show');
+    Route::patch('/boards/{slug}', [BoardController::class, 'update'])
+        ->middleware('board.access')->name('boards.update');
+    Route::delete('/boards/{slug}', [BoardController::class, 'destroy'])
+        ->middleware('board.access')->name('boards.destroy');
+
+    // Collaborators
+    Route::post('/boards/{slug}/collaborators', [BoardController::class, 'addCollaborator'])
+        ->middleware('board.access')->name('boards.collaborators.store');
+    Route::delete('/boards/{slug}/collaborators/{userId}', [BoardController::class, 'removeCollaborator'])
+        ->middleware('board.access')->name('boards.collaborators.destroy');
+
+    // Shared board access
+    Route::get('/shared/{slug}', [SharedBoardController::class, 'show'])->name('shared.show');
+    Route::post('/shared/{slug}/join', [SharedBoardController::class, 'join'])->name('shared.join');
+
+    // Todos (scoped under board)
+    Route::get('/boards/{slug}/todos/create', [TodoController::class, 'create'])
+        ->middleware('board.access')->name('todos.create');
+    Route::post('/boards/{slug}/todos', [TodoController::class, 'store'])
+        ->middleware('board.access')->name('todos.store');
+    Route::get('/boards/{slug}/todos/{todo}', [TodoController::class, 'show'])
+        ->middleware('board.access')->name('todos.show');
+    Route::get('/boards/{slug}/todos/{todo}/edit', [TodoController::class, 'edit'])
+        ->middleware('board.access')->name('todos.edit');
+    Route::patch('/boards/{slug}/todos/{todo}', [TodoController::class, 'update'])
+        ->middleware('board.access')->name('todos.update');
+    Route::delete('/boards/{slug}/todos/{todo}', [TodoController::class, 'destroy'])
+        ->middleware('board.access')->name('todos.destroy');
+
+    // CSV export (scoped under board)
+    Route::get('/boards/{slug}/todos/export', [DatabaseController::class, 'export'])
+        ->middleware('board.access')->name('todos.export');
 });
 
 require __DIR__.'/auth.php';
